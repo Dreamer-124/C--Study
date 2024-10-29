5b09c3d559b8e3933e17ff30c0bfa29303f396d0
Dreamer-124 <shuixyb@126.com>
2024-10-29 15:55:13 +0800

    2024-10-29-update

--- HelloWorld/HelloWorld/Math.cpp
+++ HelloWorld/HelloWorld/Math.cpp
@@ -1,14 +1,59 @@
 #include <iostream>
-#include "Log.h"
 
-int Multiply(int a, int b)
+#define LOG(x) std::cout << x << std::endl
+
+
+void point_heap()
 {
-	Log("Multiply");
-	return a * b;
+	char* buffer = new char[8];
+	memset(buffer, 0, 8);  // 在堆中申请内存
+
+	char** ptr = &buffer;  // 双指针
+	delete[] buffer;  // 释放内存
+}
+
+void point_stack()
+{
+	int var = 8;  // 在栈中申请内存
+	int* ptr = &var;
+	*ptr = 10;
+	LOG(var);
+}
+
+void Increment(int& value)
+{
+	value++;
+}
+
+void reference()
+{
+	int a = 5;
+	Increment(a);
+	LOG(a);
+}
+
+class Player
+{
+	public:
+		int x, y;
+		int speed;
+
+		void Move(int xa, int ya)
+		{
+			x += xa * speed;
+			y += ya * speed;
+		}
+};
+
+void class_implement()
+{
+	Player player;
+	player.Move(1, -1);
 }
 
 int main()
 {
-	std::cout << Multiply(5, 8) << std::endl;
+	class_implement();
 	std::cin.get();
 }
+ 
