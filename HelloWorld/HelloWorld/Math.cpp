23c8c8bb376926a5ec776940cb7d060b7efede66
Dreamer-124 <shuixyb@126.com>
2024-10-30 20:53:29 +0800

    2024-10-30-update

--- HelloWorld/HelloWorld/Math.cpp
+++ HelloWorld/HelloWorld/Math.cpp
@@ -2,7 +2,6 @@
 
 #define LOG(x) std::cout << x << std::endl
 
-
 void point_heap()
 {
 	char* buffer = new char[8];
@@ -51,9 +50,81 @@ void class_implement()
 	player.Move(1, -1);
 }
 
+class Log
+{
+public:
+	enum Level
+	{
+		levelError = 0, levelWarning, levelInfo
+	};
+private:
+	Level m_LogLevel = levelInfo;
+public:
+	void SetLevel(Level level)
+	{
+		m_LogLevel = level;
+	}
+	void Error(const char* message)
+	{
+		if (m_LogLevel >= levelError)
+			std::cout << "[ERROR]: " << message << std::endl;
+	}
+	void Warn(const char* message)
+	{
+		if (m_LogLevel >= levelWarning)
+			std::cout << "[WARNING]: " << message << std::endl;
+	}
+	void Info(const char* message)
+	{
+		if (m_LogLevel >= levelInfo)
+			std::cout << "[INFO]: " << message << std::endl;
+	}
+};
+
+void log()
+{
+	Log log;
+	log.SetLevel(Log::levelWarning);
+	log.Error("Hello");
+	log.Warn("Hello");
+	log.Info("Hello");
+}
+
+void Function()
+{
+	static int i = 0;  // 生存周期是程序的
+	i++;
+	std::cout << i << std::endl;
+}
+
+void local_static()
+{
+	for (int j = 0; j < 5; j++)
+	{
+		Function();
+	}
+}
+
+class Singleton
+{
+public:
+	static Singleton& Get()
+	{
+		static Singleton instance;
+		return instance;
+	}
+
+	void Hello() {}
+};
+
+void get_instance()
+{
+	Singleton::Get().Hello();
+}
+
 int main()
 {
-	class_implement();
+	log();
 	std::cin.get();
 }
  
